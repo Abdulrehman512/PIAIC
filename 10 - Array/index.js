582fcf07e00ecc1a20e01894f56c55b4b3851450
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-11 00:36:04 +0500

    Add files via upload

---
+++ 10 - Array/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let fruits = ["Apple", "Mango", "Banana", "Peach", "WaterMelon"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
