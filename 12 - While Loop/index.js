7276e5e9e3ffbaff0970699108df637be7704349
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-11 14:54:05 +0500

    Add files via upload

---
+++ 12 - While Loop/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var index = 0;
+while (index <= 10) {
+    console.log("Abdul");
+    index += 1;
+}
+var i = 0;
+while (i <= 10) {
+    console.log("2 X " + i + " = " + i);
+    i += 1;
+}
---
+++ 12 - While Loop/index.ts
@@ -0,0 +1,13 @@
+var index:number = 0;
+
+while (index <= 10) {
+    console.log("Abdul");
+    index += 1;
+}
+
+var i:number = 0;
+
+while (i <= 10) {
+    console.log("2 X " + i + " = "  + i);
+    i += 1;
+}
