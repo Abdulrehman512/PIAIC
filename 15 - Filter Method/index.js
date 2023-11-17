f8f82a38073b27ef916f17e538062a831428b33e
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-17 22:52:40 +0500

    Add files via upload

---
+++ 15 - Filter Method/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let numbers = [2, 3, 3, 4, 5, 2, 77, 4, 3, 7, 4, 388,];
+let updatedArray = numbers.filter((numbers) => true);
+console.log(updatedArray);
---
+++ 15 - Filter Method/index.ts
@@ -0,0 +1,5 @@
+let numbers:number[] = [2,3,3,4,5,2,77,4,3,7,4,388,] 
+
+let updatedArray = numbers.filter((numbers)=> true);
+
+console.log(updatedArray); 
