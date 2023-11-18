cb484515f09a835d67a4121f7b8b85e25f4e6865
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-18 11:10:09 +0500

    Add files via upload

---
+++ 16 - Objects/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var car = {
+    company: "Honda",
+    color: "White",
+    year: 2023,
+    isUsedCar: false,
+};
+console.log(car);
+var cars = [{
+        company: "Kia",
+        color: "White"
+    },
+    {
+        company: "MG",
+        color: "HS"
+    }];
+console.log(cars[1]);
---
+++ 16 - Objects/index.ts
@@ -0,0 +1,25 @@
+type carType = {company:string,isUsed:boolean,year:number,color:string}
+
+var car:carType = {
+    company : "Honda",
+    color : "White",
+    year: 2023,
+    isUsed : false,
+}
+
+console.log(car);
+  
+
+
+var cars = [{
+    company : "Kia",
+    color : "White"
+},
+{
+    company : "MG",
+    color : "HS"
+}]
+
+console.log(cars[1]);
+
+
