810c84c851ff66ef10d08058fec41610764c5996
Abdul Rehman <92779012+Abdulrehman512@users.noreply.github.com>
2023-11-19 21:32:10 +0500

    Add files via upload

---
+++ 17 - Asynchronous/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Before Async");
+function doSomething() {
+    console.log("Print Something");
+}
+// setTimeout(doSomething,5000)
+setInterval(doSomething, 5000);
+console.log("After Async");
